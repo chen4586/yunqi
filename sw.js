cd96d2a7b89617091c14d4379ff5585762fe24df
chen4586 <710844226@qq.com>
2026-09-25 12:55:48 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+/* 孕期助手 Service Worker
+ * 作用：把页面与图标缓存到本机，装一次之后即使飞行模式也能打开。
+ * 升级页面后，把下面的 CACHE 版本号 +1 即可让新版本生效。
+ */
+var CACHE = 'yunqi-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return c.addAll(ASSETS);
+    }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        return k === CACHE ? null : caches.delete(k);
+      }));
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(function (hit) {
+      if (hit) return hit;
+      return fetch(e.request).then(function (res) {
+        if (res && res.ok && res.type === 'basic') {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function () {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
